a8dde0cad1065ac26eae44fabc2deda0d390d4c1
Gustav <gustav.jakobsson@elev.ntig.se>
2026-03-04 19:12:18 +0100

    movement  done

---
+++ Assets/playerScripts/playerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Vector2 _movement;
+
+    private Rigidbody2D _rb;
+
+    private void Awake() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+
+        _rb.linearVelocity = _movement * _moveSpeed;
+    }
+}
